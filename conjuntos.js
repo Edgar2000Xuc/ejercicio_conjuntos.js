c05113d5fcce5225874a64dff7386ea57feb7181
Edgar2000Xuc <117703302+Edgar2000Xuc@users.noreply.github.com>
2023-05-12 16:51:56 +0330

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,14 @@
+//Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+
+// Creamos un Set con los nombres de tu familia
+const familia = new Set(['Luis', 'Francisco', 'Leandres']);
+
+// Añadimos tu nombre al Set original (duplicado)
+familia.add('Edgar');
+familia.add('Familia');
+
+// Añadimos el nombre "Javascript" al Set original
+familia.add('Javascript');
+
+// Imprimimos el contenido del Set en la consola
+console.log(familia);
